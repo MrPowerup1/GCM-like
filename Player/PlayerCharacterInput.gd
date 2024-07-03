a2dd9b8bb1d15550e63c546519743bd30f56cd74
JBovee <32918739+JBovee99@users.noreply.github.com>
2024-07-03 15:03:26 -0700

    Changed Input to use Fixed Numbers

--- Player/PlayerCharacterInput.gd
+++ Player/PlayerCharacterInput.gd
@@ -7,22 +7,24 @@ class_name PlayerCharacterInput
 #@export var device_id:int
 enum device_type {LOCAL,REMOTE}
 @export var device:device_type
-
-var fixed_zero_vector = SGFixedVector2.new()
+const straight_fixed:int = 65536
+const diag_fixed:int = 46341
+var fixed_zero_vector = SGFixed.vector2(0,0)
 
 signal button_activate(index:int)
 signal button_release(index:int)
 signal direction_pressed(direction:Vector2)
 
 
-func _ready():
-	fixed_zero_vector.from_float(Vector2.ZERO)
-
-
 func _get_local_input() -> Dictionary:
-	var input_vector = SGFixedVector2.new()
 	var input = {}
-	input_vector.from_float(Input.get_vector(input_keys.conversion["Left"],input_keys.conversion["Right"],input_keys.conversion["Up"],input_keys.conversion["Down"]))
+	
+	#Doesn't work because of floats
+	#var input_vector = SGFixedVector2.new()
+	#input_vector.from_float(Input.get_vector(input_keys.conversion["Left"],input_keys.conversion["Right"],input_keys.conversion["Up"],input_keys.conversion["Down"]))
+	
+	var input_vector = input_axis()
+	
 	if (Input.is_action_just_pressed(input_keys.conversion["Spell1"])):
 		input['spell_1_pressed']=true
 	if (Input.is_action_just_released(input_keys.conversion["Spell1"])):
@@ -33,9 +35,30 @@ func _get_local_input() -> Dictionary:
 		input['spell_2_released']=true
 	#print(input_vector.x, " (input) compared to (zero) ",fixed_zero_vector.x, " Equal? ",input_vector.is_equal_approx(fixed_zero_vector))
 	if not input_vector.is_equal_approx(fixed_zero_vector):
-		input['input_vector']=input_vector
-		
+		input['input_vector']=input_vector	
 	return input
+
+func input_axis() -> SGFixedVector2:
+	var horizontal_sum = 0
+	var vertical_sum = 0
+	if Input.is_action_pressed(input_keys.conversion["Left"]):
+		horizontal_sum -=1
+	if Input.is_action_pressed(input_keys.conversion["Right"]):
+		horizontal_sum +=1
+	if Input.is_action_pressed(input_keys.conversion["Up"]):
+		vertical_sum -=1
+	if Input.is_action_pressed(input_keys.conversion["Down"]):
+		vertical_sum +=1
+
+	if horizontal_sum !=0:
+		if vertical_sum == 0:
+			return SGFixed.vector2(horizontal_sum*straight_fixed,0)
+		else:
+			return SGFixed.vector2(horizontal_sum*diag_fixed,vertical_sum*diag_fixed)
+	elif vertical_sum !=0:
+		return SGFixed.vector2(0,vertical_sum*straight_fixed)
+	else:
+		return SGFixed.vector2(0,0)
 
 func _network_postprocess(input: Dictionary) -> void:
 	var move_vector = input.get('input_vector',fixed_zero_vector) #SGFixed.vector2(input.get('input_vector_x', 0),input.get('input_vector_y', 0))
@@ -48,6 +71,7 @@ func _network_postprocess(input: Dictionary) -> void:
 		button_activate.emit(1)
 	if input.get('spell_2_released',false):
 		button_release.emit(1)
+
 func _predict_remote_input(previous_input:Dictionary, ticks_since_last_input: int) -> Dictionary:
 	var input = previous_input.duplicate()
 	input.erase('spell_1_pressed')
